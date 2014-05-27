955d9d8ca26fbc803594ea4bf0be5fa75d3e944f
roripika <roripika@gmail.com>
2014-05-27 11:15:38 +0900

    環境変更

--- rendaPower/proj.android/jni/Application.mk
+++ rendaPower/proj.android/jni/Application.mk
@@ -1,2 +1,5 @@
 APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -DCOCOS2D_DEBUG=1
+# Release.
+APP_CPPFLAGS := -frtti
+# Debug. Need this in order to output CCLOG.
+# APP_CPPFLAGS := -frtti -DCOCOS2D_DEBUG=1
