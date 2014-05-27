a17b7363378e00eb7338e8526294bab0b511dc21
大原 幸夫 殿 <oohara@wonderpla.net>
2014-05-27 11:06:54 +0900

    デバッグ用

--- rendaPower/Classes/AppDelegate.cpp
+++ rendaPower/Classes/AppDelegate.cpp
@@ -77,9 +77,10 @@ bool AppDelegate::applicationDidFinishLaunching()
             break;
     }
     
-    
+#if COCOS2D_DEBUG
     // turn on display FPS
     pDirector->setDisplayStats(true);
+#endif
 
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
