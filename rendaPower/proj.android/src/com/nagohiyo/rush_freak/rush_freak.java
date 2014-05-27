3e376996f6be100362cd93b8b2a3fd6bcd6f4a7c
roripika <roripika@gmail.com>
2014-05-27 11:36:01 +0900

    名前変更

--- rendaPower/proj.android/src/com/nagohiyo/rush_freak/rush_freak.java
+++ rendaPower/proj.android/src/com/nagohiyo/rush_freak/rush_freak.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package com.example.rush_freak;
+package com.nagohiyo.rush_freak;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
